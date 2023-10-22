c1d02202f24b28d6f50860dd698f3c6c4ecfc46b
Olitz <100507490+Olitz@users.noreply.github.com>
2023-10-22 22:04:54 +0330

    Add files via upload

--- php/index.php
+++ php/index.php
@@ -1,1 +1,26 @@
+<?php
+include 'prct.php';
+$conn = OpenCon();
+echo "Connected Successfully";
 
+
+$sql = "INSERT INTO `messages`(`title`, `body`) VALUES ('Test111','2Hiiiiii111111222iiiii');";
+
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$sql = "INSERT INTO `users`(`name`, `email`, `phone_number`) VALUES ('omid ssssh','jsss4k@gmail.com','0922162415516');";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+CloseCon($conn);
+
+?>
