4684851b5e5cbbc6e2e8ff65cad8804474bc8a04
Olitz <100507490+Olitz@users.noreply.github.com>
2023-10-28 20:57:53 +0330

    Add files via upload

--- t4/product.php
+++ t4/product.php
@@ -1,1 +1,31 @@
+<?php
+include 'prct.php';
+$conn = OpenCon();
+echo "Connected Successfully";
 
+$sql = "INSERT INTO `product`( `title`, `description`, `price`, `category`, `color`, `count`) VALUES ('picher','barista space picher','750000','coffee shop','red','10')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully ";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $sql = "UPDATE `product` SET `description`='edited',`price`='400000',`is_deleted`='0' WHERE p_id=4 ";
+
+if ($conn->query($sql) === TRUE) {
+    echo " edited";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $sql = "UPDATE `product` SET `is_deleted`='1' WHERE p_id=4 ";
+
+  if ($conn->query($sql) === TRUE) {
+      echo " soft delete";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+CloseCon($conn);
+
+?>
---
+++ t4/select.sql
@@ -0,0 +1,2 @@
+SELECT * FROM `product`;
+SELECT `p_id`, `title`, `description`, `price`, `category`, `color`, `image`, `count` FROM `product` WHERE p_id=15
